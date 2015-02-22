51c234e1b8c925b821cde62511bb053286531410
batermj <bater.makhabel@gmail.com>
2015-02-23 03:48:15 +0800

    add one line to test the cart classifier

--- chapter3/ch_03_cart.R
+++ chapter3/ch_03_cart.R
@@ -8,13 +8,12 @@ data <- read.csv("data/datasets003.csv", header=TRUE)
 data <- as.data.frame(data)
 
 ct <- rpart.control(xval=10, minsplit=20, cp=0.1) 
-
-fit <- rpart(Kyphosis~Age + Number + Start, 
-		data=data, 
+formula <- Kyphosis~Age + Number + Start
+fit <- rpart(formula=formula,data=data, 
 		method="class",control=ct,
-		parms = list(prior = c(0.65,0.35), 
-		split = "information"))
+		parms=list(prior = c(0.65,0.35), 
+		split="information"))
 
 print(fit)
-ndata=data.frame('Kyphosis'='present', 'Age'=82, 'Number'=5, 'Start'=11)
+ndata=data.frame('Kyphosis'='present','Age'=82,'Number'=5,'Start'=11)
 print(predict(fit,ndata))

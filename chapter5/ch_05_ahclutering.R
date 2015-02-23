19d90f0b1a66a98baff9bac6529ded8c6e7b65a9
batermj <bater.makhabel@gmail.com>
2015-02-23 18:54:32 +0800

    update the parameter

--- chapter5/ch_05_ahclutering.R
+++ chapter5/ch_05_ahclutering.R
@@ -4,11 +4,11 @@ gc()
 
 data <- read.csv("data/datasets009.csv", header=TRUE)
 
-dist.e=dist(data,method='manhattan')
+dist.e <- dist(data,method='manhattan')
 
-model1=hclust(dist.e,method='ward')
-result=cutree(model1,k=3)
+model2ah <- hclust(dist.e,method='ward')
+result <- cutree(model2ah,k=3)
 
 print("Agglomerative Hierachical Clustering")
-print(model1)
+print(model2ah)
 print(result)

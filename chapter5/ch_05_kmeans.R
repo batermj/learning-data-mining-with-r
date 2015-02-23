efbcd1dd2cb0c8ce2d12dee75fe3faf1f5326007
batermj <bater.makhabel@gmail.com>
2015-02-23 18:09:31 +0800

    update with new parameter for pamk

--- chapter5/ch_05_kmeans.R
+++ chapter5/ch_05_kmeans.R
@@ -5,12 +5,13 @@ gc()
 library(fpc)
 
 data <- read.csv("data/datasets001.csv",header=TRUE)
-print(data)
 
 pamk.result <- pamk(data)
-print(pamk.result)
-print(pamk.result$nc)
-
 kc <- kmeans(data, pamk.result$nc)
-kc$cluster
+print(kc$cluster)
+print(kc)
+
+pamk.result <- pamk(data,criterion="multiasw",usepam=FALSE,critout=TRUE)
+kc <- kmeans(data, pamk.result$nc)
+print(kc$cluster)
 print(kc)

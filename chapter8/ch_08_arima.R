90adb3352acdd11237baca4b891c9ac27a37cf21
batermj <bater.makhabel@gmail.com>
2015-02-25 18:30:55 +0800

    update the algorithm of arima

--- chapter8/ch_08_arima.R
+++ chapter8/ch_08_arima.R
@@ -6,10 +6,10 @@ library(forecast)
 library(zoo)
 library(xts)
 
-#data003 <- read.csv("data/datasets006.csv", header=TRUE)
-data003 <- WWWusage
+data003 <- read.csv("data/datasets003.csv",header=T)
+data003 <- ts(c(data003[,1]))
 
-arima.model <- Arima(data003,order=c(3,1,0),fixed=c(0,NA,0,NA))
+arima.model <- arima(data003,order=c(3,1,0),fixed=c(0,NA,0,NA))
 tsdiag(arima.model)
 print(arima.model)
 forecast(arima.model,h=20)
